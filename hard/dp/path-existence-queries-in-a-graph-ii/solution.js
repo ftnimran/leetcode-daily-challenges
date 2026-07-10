edd6721d2eb9bceb2b9a50d65a4d0b99d15278b4
Imran Ali <109467587+ftnimran@users.noreply.github.com>
2026-07-10 21:06:15 +0530

    Added Path Existence Queries in a Graph II | hard | JavaScript

--- hard/dp/path-existence-queries-in-a-graph-ii/solution.js
+++ hard/dp/path-existence-queries-in-a-graph-ii/solution.js
@@ -6,45 +6,81 @@
  * @return {number[]}
  */
 var pathExistenceQueries = function (n, nums, maxDiff, queries) {
-  // Build Graph
-  const graph = Array.from({ length: n }, () => []);
+  const arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push([nums[i], i]);
+  }
+
+  arr.sort((a, b) => a[0] - b[0]);
+
+  const pos = new Array(n);
+  const comp = new Array(n);
+
+  let cid = 0;
 
   for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      if (Math.abs(nums[i] - nums[j]) <= maxDiff) {
-        graph[i].push(j);
-        graph[j].push(i);
-      }
+    if (i && arr[i][0] - arr[i - 1][0] > maxDiff) cid++;
+
+    pos[arr[i][1]] = i;
+    comp[arr[i][1]] = cid;
+  }
+
+  const reach = new Array(n);
+
+  let r = 0;
+
+  for (let l = 0; l < n; l++) {
+    while (r + 1 < n && arr[r + 1][0] - arr[l][0] <= maxDiff) {
+      r++;
+    }
+
+    reach[l] = r;
+  }
+
+  const LOG = 17;
+
+  const up = Array.from({ length: LOG }, () => new Array(n));
+
+  for (let i = 0; i < n; i++) up[0][i] = reach[i];
+
+  for (let k = 1; k < LOG; k++) {
+    for (let i = 0; i < n; i++) {
+      up[k][i] = up[k - 1][up[k - 1][i]];
     }
   }
 
-  function dfs(node, target, vis) {
-    if (node === target) return 0;
-
-    vis[node] = true;
-
-    let ans = Infinity;
-
-    for (const next of graph[node]) {
-      if (!vis[next]) {
-        const d = dfs(next, target, vis);
-
-        if (d !== Infinity) ans = Math.min(ans, d + 1);
-      }
-    }
-
-    return ans;
-  }
-
-  const res = [];
+  const ans = [];
 
   for (const [u, v] of queries) {
-    const vis = new Array(n).fill(false);
+    if (u === v) {
+      ans.push(0);
+      continue;
+    }
 
-    const dist = dfs(u, v, vis);
+    if (comp[u] !== comp[v]) {
+      ans.push(-1);
+      continue;
+    }
 
-    res.push(dist === Infinity ? -1 : dist);
+    let l = pos[u];
+    let rr = pos[v];
+
+    if (l > rr) {
+      [l, rr] = [rr, l];
+    }
+
+    let cur = l;
+    let steps = 0;
+
+    for (let k = LOG - 1; k >= 0; k--) {
+      if (up[k][cur] < rr) {
+        cur = up[k][cur];
+        steps += 1 << k;
+      }
+    }
+
+    ans.push(steps + 1);
   }
 
-  return res;
+  return ans;
 };
